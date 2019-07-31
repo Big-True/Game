75d0c12bcd701e2f8265a36e84797ace61579d79
Big_True <big_true@qq.com>
2019-07-31 22:31:52 +0800

    就剩下按键响应 和消除判定了

--- game.py
+++ game.py
@@ -4,21 +4,40 @@ pygame.init()
 logo = pygame.image.load(r'./res/1.jpg')
 pygame.display.set_icon(logo)
 pygame.display.set_caption('东方大瞎眼')
+clock=pygame.time.Clock()
+size = 100
 screen = pygame.display.set_mode((800, 600))
 images = list([pygame.transform.scale(pygame.image.load(
-    r'./res/%d.jpg' % m), (50, 50)) for m in range(1, 21)])
-map = list([[-1 if int((m+n*16) % 50/2)+1>20 else int((m+n*16) % 50/2)+1 for m in range(16)] for n in range(12)])
-for a in range(1000):
-    i = [random.randint(0, 11), random.randint(0, 15)]
-    j = [random.randint(0, 11), random.randint(0, 15)]
-    map[i[0]][i[1]], map[j[0]][j[1]] = map[j[0]][j[1]], map[i[0]][i[1]]
-print(map)
+    r'./res/%d.jpg' % m), (size, size)) for m in range(1, 21)])
+map = []
+
+
+def resize():
+    size = size/2
+
+
+def newgame():
+    map[:] = list([[-1 if m+n*(800/size)>(480000/size/size/3*2) else int(((m+n*(800/size)) %
+                                                                       40)/2)+1 for m in range(int(800/size))] for n in range(int(600/size))])
+    for a in range(int(100000/size)):
+        i = [random.randint(0, (600/size)-1), random.randint(0, (800/size)-1)]
+        j = [random.randint(0, (600/size)-1), random.randint(0, (800/size)-1)]
+        map[i[0]][i[1]], map[j[0]][j[1]] = map[j[0]][j[1]], map[i[0]][i[1]]
+
+
+newgame()
 while True:
+    clock.tick(60)
     screen.fill((255, 255, 255))
-    for i in range(0, 16):
-        for j in range(0, 12):
+    temp=0
+    for i in range(0, int(800/size)):
+        for j in range(0, int(600/size)):
             if map[j][i] >= 0:
-                screen.blit(images[map[j][i]-1], (i*50, j*50))
+                screen.blit(images[map[j][i]-1], (i*size, j*size))
+                temp=temp+1
+    if temp==0:
+        resize()
+        newgame()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
